bb7f32817ebb678eb20871b24f7a75d767c94029
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-08-22 09:24:57 +0200

    update doi to download latest version of DB

--- code/download_data.R
+++ code/download_data.R
@@ -2,6 +2,6 @@
 # download latest version of the database from Zenodo, https://zenodo.org/doi/10.5281/zenodo.10782428
 
 cat("Downloading raw data\n")
-zen4R::download_zenodo("10.5281/zenodo.10782428", path = "data/")
+zen4R::download_zenodo("10.5281/zenodo.13358435", path = "data/")
 
 cat("Data successfully downloaded! \n")
